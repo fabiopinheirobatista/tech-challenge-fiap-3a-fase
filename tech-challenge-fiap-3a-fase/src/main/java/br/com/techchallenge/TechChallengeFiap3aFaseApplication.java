e34c30449a0dfb131a39adb13ba6910c8a7d0062
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-28 20:33:46 -0300

    Second commit

---
+++ tech-challenge-fiap-3a-fase/src/main/java/br/com/techchallenge/TechChallengeFiap3aFaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeFiap3aFaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeFiap3aFaseApplication.class, args);
+	}
+
+}
