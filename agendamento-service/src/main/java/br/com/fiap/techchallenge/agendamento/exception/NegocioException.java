1b7c62f8eddfa58a9af84f4a92daedae2bee3b71
Fabio <fabiobatista.bertioga@gmail.com>
2025-05-27 01:02:24 -0300

    Ajustando branch para receber desenvolvimento correto

--- agendamento-service/src/main/java/br/com/fiap/techchallenge/agendamento/exception/NegocioException.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.techchallenge.agendamento.exception;
-
-public class NegocioException extends RuntimeException {
-
-    public NegocioException(String message) {
-        super(message);
-    }
-
-}
--- agendamento-service/src/main/resources/db/migration/V003__popular_dados.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE consultas
-    MODIFY COLUMN status ENUM('agendada', 'realizada', 'cancelada');
